01e31e0760b2d4afffd81d18b980a1aa20886b34
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-09-07 15:59:07 +0500

    Task1

---
+++ Tyuiu.MedvedvKA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MedvedvKA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Кирилл";
+            int age = 20;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+
+        }
+    }
+}
