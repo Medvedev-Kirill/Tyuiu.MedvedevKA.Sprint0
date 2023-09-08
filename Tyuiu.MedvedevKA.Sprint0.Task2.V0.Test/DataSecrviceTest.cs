7a5a4476c0d90d3403431cd052c19f65e4aa7f3d
Tyuiu.MedvedevKA@outlook.com <medvedevkirill2016@mail.ru>
2023-09-08 16:02:54 +0500

    Task2

---
+++ Tyuiu.MedvedevKA.Sprint0.Task2.V0.Test/DataSecrviceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MedvedevKA.Sprint0.Task2.V0.Lib; 
+namespace Tyuiu.MedvedevKA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataSecrviceTest
+    {
+        [TestMethod]
+        public void CheckGetMassageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Кирилл";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Кирилл", res);
+        }
+    }
+}
---
+++ Tyuiu.MedvedevKA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MedvedevKA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.MedvedevKA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMessage
+            //из библиотеки Tyuiu.MedvedevKA.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Кирилл"));
+            Console.ReadKey();
+        }
+    }
+}
